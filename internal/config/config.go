e6d99b03f9582b49794f121dff0635a5d630837e
DL1793 <mihainiculescuc@gmail.com>
2026-03-31 19:50:35 +0300

    Added config json

---
+++ internal/config/config.go
@@ -0,0 +1,38 @@
+package config
+
+import (
+	"encoding/json"
+	"os"
+)
+
+const configFileName = ".gatorconfig.json"
+
+type Config struct {
+	DB_Url            string `json:"db_url"`
+	Current_User_Name string `json:"current_user_name"`
+}
+
+func Read() (Config, error) {
+	jsonFile, err := os.ReadFile(configFileName)
+	if err != nil {
+		return Config{}, err
+	}
+	var config Config
+	err = json.Unmarshal(jsonFile, &config)
+	if err != nil {
+		return Config{}, err
+	}
+	
+}
+
+func (c *Config) SetUser() {
+
+}
+
+func getConfigFilePath() (string, error) {
+
+}
+
+func write(cfg Config) error {
+
+}
